f8c233a741c6a091fd8822ce3bb52c906cd105bf
MOISKY <61997974+MOISKY@users.noreply.github.com>
2022-10-19 20:13:24 -0500

    Add files via upload

---
+++ cambia.js
@@ -0,0 +1,6 @@
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+document.write('<input id = "ireferencia name = "referencia" value ="'+random(1,5)+'"/>');
