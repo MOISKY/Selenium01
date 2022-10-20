da9e442192c17121fad3a37b4a4fd38a789d3e24
MOISKY <61997974+MOISKY@users.noreply.github.com>
2022-10-19 21:22:50 -0500

    Update cambia.js

--- cambia.js
+++ cambia.js
@@ -3,4 +3,4 @@ function random(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-document.write('<input id = "ireferencia name = "referencia" value ="'+random(1,5)+'"/>');
+document.write('<input id = "ireferencia" name = "referencia" value ="'+random(1,5)+'"/>');
